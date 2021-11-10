72e3fbcdcf7cbac1d481d7a4b0d6dd587a8da754
Diki Rahman <dikirahman.me@gmail.com>
2021-11-10 09:04:13 +0700

    init

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,12 @@ WORKDIR /var/www/app
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
 COPY package.json ./
-COPY app.js ./
-COPY models ./
+RUN npm install
+COPY . ./
 
-RUN npm install -g yarn forever --force && \
-  yarn install --production --force
 # If you are building your code for production
 # RUN npm ci --only=production
 
-USER node
 EXPOSE 3001
 
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -1,111 +1,24 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
+const mongodb = require('mongodb');
 
-const port = process.env.PORT || 3001;
+const config = require('./db');
+const PORT = 3001;
+const client = mongodb.MongoClient;
 
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-const User = require("./models/user"); // import our declared schema  from models folder
-const mongoose = require("mongoose"); // import mongoose
-mongoose.Promise = global.Promise; // configure mongoose promises
-app.use(require("morgan")("dev"));
-const mongodbAPI = "mongodb://127.0.0.1:27017/mongoexpress"; // here we declare our database URL. we can use any name after instead of "mongoexpress"  and it will automatically create database for us in that name
-
-app.post("/adduser", (req, res) => {
-  var newUser = User({
-    // create a new user object and pass the formdata to the newUser . then call .save() method . it will return a promise .
-    email: req.body.email,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    address: req.body.address,
-    Occupation: req.body.Occupation,
-    income: req.body.income
-  });
-
-  newUser
-    .save()
-    .then(doc => {
-      console.log(doc);
-      res.json(doc);
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(err);
-    });
-});
-
-app.get("/viewusers", (req, res) => {
-  User.find({}) // find without any conditions will return all the documents in that collection
-    .then(docs => {
-      res.json(docs); // send documents
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
-
-app.post("/deletebyid", (req, res) => {
-  User.findByIdAndDelete(req.body.userid)
-    .then(result => {
-      res.json({ status: "ok", result: result });
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(err);
-    });
-});
-
-app.post("/deletebyemail", (req, res) => {
-  User.deleteOne({ email: req.body.email })
-    .then(result => {
-      res.json({ status: "ok", result: result });
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(err);
-    });
-});
-
-app.post("/updateuser", (req, res) => {
-  User.findByIdAndUpdate(req.body.userid, {
-    $set: {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      address: req.body.address,
-      Occupation: req.body.Occupation,
-      income: req.body.income
+client.connect(config.DB, function(err, db) {
+    if(err) {
+        console.log('database is not connected')
     }
-  })
-    .then(doc => {
-      res.json({ status: "ok", result: doc });
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(err);
-    });
-});
-
-try {
-  // here we connect with MongoDB with the url we specified earlier
-  mongoose.connect(
-    mongodbAPI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    err => {
-      if (!err) console.log("connected to mongodb sucsessfully" + "👍");
-      console.log(err);
+    else {
+        console.log('connected!!')
     }
-  );
-} catch (error) {
-  console.log(error);
-}
-
-app.listen(port, () => {
-  console.log("listning on " + port);
 });
 
+app.get('/', function(req, res) {
+    res.json({"hello": "world"});
+});
 
-User.findOneAndDelete
+app.listen(PORT, function(){
+    console.log('Your node js server is running on PORT:',PORT);
+});
